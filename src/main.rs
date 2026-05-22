501ac9357ac43c9ff0fdd1b4f4552f4e08c93fa4
sharayujosh <61659558+sharayujosh@users.noreply.github.com>
2026-05-21 20:42:24 -0400

    test text match w/ struct/impl

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,35 @@
-use clap::Parser;
+// use clap::Parser;
+// use serde_json::Value;
+// use std::fs;
+
+struct ExactTextFilter {
+    pattern: String,
+}
+
+impl ExactTextFilter {
+    fn new(x: &str) -> Self {
+        Self {
+            pattern:String::from(x)
+        }
+    }
+
+    fn equals(&self, x: &str) -> bool {
+        x == self.pattern
+    }
+}
+
+// filters on severity of vulnerability
+// fn severity(vex_data: &Value, &str severity: String) -> Self {
+//     // Implementation for severity filter
+//     let vuln = vex_data.get("vulnerabilities")?.as_array()?;
+// }
+
+// unit test of 'severity' function for VEX files
+// fn 
+
+fn main() {
+    let tf = ExactTextFilter::new("foo");
+    println!("{}", tf.equals("bar"));
+
+    //let sample_data = serde_json::from_str(&(fs::read_to_string("sample_vex.json")?))?;
+}
