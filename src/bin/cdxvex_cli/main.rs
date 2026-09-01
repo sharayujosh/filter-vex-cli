0a5e8d28054c629eaccb0c5c29b0b3540a3e4ec2
sharayujosh <61659558+sharayujosh@users.noreply.github.com>
2026-09-01 17:38:27 -0400

    add filter creation logic

--- src/bin/cdxvex_cli/main.rs
+++ src/bin/cdxvex_cli/main.rs
@@ -8,7 +8,7 @@
     )
 )]
 
-use clap::{Command, Parser, Subcommand};
+use clap::{Parser, Subcommand};
 use filter_vex_cli::{CdxVex, CdxVexFilter, create_filter};
 use miette::{IntoDiagnostic, Result};
 
@@ -46,6 +46,9 @@ enum Commands {
 }
 fn main() -> Result<()> {
     let args = Args::parse();
+    let mut obj: CdxVex = CdxVex::from_json_file(&args.input_file).into_diagnostic()?;
+    let mut filter: CdxVexFilter = CdxVexFilter::new().into_diagnostic()?;
+
     match args.command {
         Commands::Filter {
             last_updated_filter,
@@ -53,12 +56,45 @@ fn main() -> Result<()> {
             published_filter,
             updated_filter,
             created_filter,
-        } => {}
+        } => {
+            if let Some(date_filter) = last_updated_filter {
+                for f in date_filter {
+                    filter
+                        .last_updated
+                        .push(create_filter(&f.as_str()).into_diagnostic()?);
+                }
+            }
+            if let Some(date_filter) = first_issued_filter {
+                for f in date_filter {
+                    filter
+                        .first_issued
+                        .push(create_filter(&f.as_str()).into_diagnostic()?);
+                }
+            }
+            if let Some(date_filter) = published_filter {
+                for f in date_filter {
+                    filter
+                        .published
+                        .push(create_filter(&f.as_str()).into_diagnostic()?);
+                }
+            }
+            if let Some(date_filter) = updated_filter {
+                for f in date_filter {
+                    filter
+                        .updated
+                        .push(create_filter(&f.as_str()).into_diagnostic()?);
+                }
+            }
+            if let Some(date_filter) = created_filter {
+                for f in date_filter {
+                    filter
+                        .created
+                        .push(create_filter(&f.as_str()).into_diagnostic()?);
+                }
+            }
+        }
     }
 
-    // let mut obj: CdxVex = CdxVex::from_json_file(&args.input_file).into_diagnostic()?;
-
-    // let mut filter: CdxVexFilter = CdxVexFilter::new().into_diagnostic()?;
     // if let Some(date_filter) = args.last_updated_filter {
     //     for f in date_filter {
     //         filter
@@ -94,8 +130,8 @@ fn main() -> Result<()> {
     //             .push(create_filter(&f.as_str()).into_diagnostic()?);
     //     }
     // }
-    // obj.apply_filter(&filter).into_diagnostic()?;
-    // obj.write_json_file(&args.output_file).into_diagnostic()?;
+    obj.apply_filter(&filter).into_diagnostic()?;
+    obj.write_json_file(&args.output_file).into_diagnostic()?;
 
     Ok(())
 }
